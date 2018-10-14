d698af96058f8bd915ef25df28c7dc99920d58fb
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-10-15 00:18:08 +0500

    Solution for HTTP CLIENT task from learnyounode was added

---
+++ src/learnyounode/http-client/program.js
@@ -0,0 +1,9 @@
+let http = require("http");
+
+http.get(process.argv[2],
+    response => {
+        response.setEncoding("utf8");
+        response.on("data", console.log);
+        response.on("error", console.log);
+    })
+    .on("error", console.error);
