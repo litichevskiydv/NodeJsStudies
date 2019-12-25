38db11e7eaab85524e16039a37f553835f8334a9
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-12-25 11:41:54 +0300

    Includes were added

--- src/concepts/pbf/index.js
+++ src/concepts/pbf/index.js
@@ -1,12 +1,6 @@
 const path = require("path");
-const compile = require("pbf/compile");
 
-const { loadSync } = require("./schemeLoader");
+const { loadSync } = require("./packageDefinitionLoader");
 
-const protoScheme = loadSync(path.join(__dirname, "./example.proto"), [
-  path.join(__dirname, "./include/"),
-  path.join(__dirname, "../../../node_modules/grpc-tools/bin/")
-]);
-const HelloRequest = compile(protoScheme).HelloRequest;
-
-console.log(HelloRequest);
+const packageDefinition = loadSync(path.join(__dirname, "./example.proto"));
+console.log(packageDefinition);
